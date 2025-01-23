b6923ef1ab9e76f5bb0bd3b4cb9110079801b73f
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-01-23 22:16:40 +0200

    first push, busy with navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} className="w-36" alt="" />
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
+
+        <NavLink to="/" className="flex flex-col items-center">
+            <p>HOME</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/collection" className="flex flex-col items-center">
+            <p>COLLECTION</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/about" className="flex flex-col items-center">
+            <p>ABOUT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/contact" className="flex flex-col items-center">
+            <p>CONTACT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+      </ul>
+
+      <div className="flex items-center gap-6">
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        
+        <div className="group relative">
+            <img  className="w-5 cursor-pointer" src={assets.profile_icon} alt="" />
+            <div className="group hover:block hidden absolute dropdown-menu right-0 pt-4">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded "></div>
+
+            </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
